142056c3de9da1e193aa998726f4e2b044d8db1f
Qian Gao <68983944+gaoqian310@users.noreply.github.com>
2021-12-08 11:37:06 -0700

    Update Create_Tables.sql

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -5,17 +5,17 @@ CREATE TABLE Books
     Book_Id int PRIMARY KEY,  
     Book_Title varchar(100),  
     Author varchar(100),    
-    Publication_year int,
+    Publication_Date DATE,
     Stock int
 )  
 
 INSERT INTO Books
 VALUES
-    (1, 'The Catcher in the Rye','Bob Brown',2000-03-11,5),
-    (2, 'Nine Stories','Dan Brown',1999-03-11,4),
-    (3, 'Franny and Zooey','Bob Brown',2000-04-11,3),
-    (4, 'The Great Gatsby','Dan Brown',1999-04-11,2),
-    (5, 'Programming Concept','Danis Brown',1998-11-11,10)
+    (1, 'The Catcher in the Rye','Bob Brown','2000-03-11',5),
+    (2, 'Nine Stories','Dan Brown','1999-03-11',4),
+    (3, 'Franny and Zooey','Bob Brown','2000-04-11',3),
+    (4, 'The Great Gatsby','Dan Brown','1999-04-11',2),
+    (5, 'Programming Concept','Danis Brown','1998-11-11',10)
 
 Create TABLE Members  
 (  
