8f3ecdb6e2ea306cf10701be4acc117d1f6aa053
Qian Gao <68983944+gaoqian310@users.noreply.github.com>
2021-12-08 08:53:09 -0700

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,23 +1,27 @@
 # Create tables to include required columns.
 
-CREATE TABLE Books (
-    Id INT NOT NULL AUTO_INCREMENT,
-    Title VARCHAR(50) NOT NULL,
-    PRIMARY KEY(Id)
-);
+CREATE TABLE Books  
+(  
+    Book_Id int PRIMARY KEY,  
+    Book_Title varchar(100),  
+    Author varchar(100),    
+    Publication_year int,
+    Stock int
+)  
 
 INSERT INTO Books
-    (Id, Title)
 VALUES
-    (1, 'The Catcher in the Rye'),
-    (2, 'Nine Stories'),
-    (3, 'Franny and Zooey'),
-    (4, 'The Great Gatsby'),
-    (5, 'Tender id the Night'),
-    (6, 'Pride and Prejudice'),
-    (7, 'Professional ASP.NET 4.5 in C# and VB')
-
-
-# Find books written by ""
-
-
+    (1, 'The Catcher in the Rye','Bob Brown',2000-03-11,5),
+    (2, 'Nine Stories','Dan Brown',1999-03-11,4),
+    (3, 'Franny and Zooey','Bob Brown',2000-04-11,3),
+    (4, 'The Great Gatsby','Dan Brown',1999-04-11,2),
+    (5, 'Programming Concept','Danis Brown',1998-11-11,10)
+  
+CREATE TABLE Borrowings  
+(  
+    Borrowings_Id int PRIMARY KEY,  
+    Book_Id int,
+    Member_Id int,
+    Borrowed_From date,  
+    Borrowed_To date  
+)  
