9d9bfbd9cbc12d762951176ba53841509a080e94
Qian Gao <68983944+gaoqian310@users.noreply.github.com>
2021-12-08 10:45:12 -0700

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -5,4 +5,8 @@ FROM Books
 WHERE Author='Dan Brown';
 
 
-# 
+# Find the names of all books written by "Dan Brown" that were borrowed, along with the date of return
+
+SELECT Books.Book_Title AS "Title", Borrowings.Returned_Date AS "Return Date"
+FROM Borrowings JOIN Books ON Borrowings.Book_Id=Books.Book_Id
+WHERE Books.Author='Dan Brown';
