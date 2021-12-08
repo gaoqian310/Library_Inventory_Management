f7a206241bf8ded2af8b749b937d58038e134e83
Qian Gao <68983944+gaoqian310@users.noreply.github.com>
2021-12-07 22:32:07 -0700

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,1 +1,17 @@
+create table book_details (
+    bookid primary key
+    book_title varchar2(100), 
+    author varchar2(100), 
+    data_of_publication date, 
+    stock_available int,
+    );
 
+create table members (
+    first_name varchar(50)
+    last_name varcha(50)
+    );
+    
+create table borrowings (
+    first_name varchar(50)
+    last_name varcha(50)
+    );
